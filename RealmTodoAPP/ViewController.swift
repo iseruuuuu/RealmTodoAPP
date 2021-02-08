9c00a9185596eda01a7aed717b552fa00335eef7
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-02-08 13:11:32 +0900

    Initial Commit

---
+++ RealmTodoAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTodoAPP
+//
+//  Created by 井関竜太郎 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
